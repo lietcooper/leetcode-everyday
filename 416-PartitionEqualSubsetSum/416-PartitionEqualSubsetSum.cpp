8b04623963e648f5bdd9b63cb572aa0040be248d
lijunwan <114791855+lietcooper@users.noreply.github.com>
2025-12-09 15:28:52 +0800

    Create 416-PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -0,0 +1,21 @@
+// Last updated: 12/8/2025, 11:28:52 PM
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+        }
+        if (sum % 2) return false;
+        int target = sum / 2;
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = target; j >= nums[i]; --j) {
+                dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+};
