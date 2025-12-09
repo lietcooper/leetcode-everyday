4a1eb32a5734ed7c7f74a767c9d25099fb902bc0
lijunwan <114791855+lietcooper@users.noreply.github.com>
2025-12-09 15:28:05 +0800

    Create 658-FindKClosestElements

---
+++ 658-FindKClosestElements/658-FindKClosestElements.cpp
@@ -0,0 +1,17 @@
+// Last updated: 12/8/2025, 11:28:05 PM
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size() - k;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return vector<int>(arr.begin() + left, arr.begin() + left + k);
+    }
+};
