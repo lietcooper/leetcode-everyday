61059372ee6f9fcab642a75495a18b39e932a747
lijunwan <114791855+lietcooper@users.noreply.github.com>
2025-12-09 15:25:42 +0800

    Create 2497-MaximumMatchingOfPlayersWithTrainers

---
+++ 2497-MaximumMatchingOfPlayersWithTrainers/2497-MaximumMatchingOfPlayersWithTrainers.cpp
@@ -0,0 +1,16 @@
+// Last updated: 12/8/2025, 11:25:42 PM
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int i = 0, j = 0;
+        while (i < players.size() && j < trainers.size()) {
+            if (players[i] <= trainers[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
