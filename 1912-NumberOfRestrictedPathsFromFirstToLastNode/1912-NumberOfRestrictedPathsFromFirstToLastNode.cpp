54599cb87d0d3e38fcaf5c28630856e2c9e0cbf8
lijunwan <114791855+lietcooper@users.noreply.github.com>
2025-12-09 15:25:59 +0800

    Create 1912-NumberOfRestrictedPathsFromFirstToLastNode

---
+++ 1912-NumberOfRestrictedPathsFromFirstToLastNode/1912-NumberOfRestrictedPathsFromFirstToLastNode.cpp
@@ -0,0 +1,37 @@
+// Last updated: 12/8/2025, 11:25:59 PM
+class Solution {
+public:
+    int countRestrictedPaths(int n, vector<vector<int>>& edges) {
+        const int MOD = 1000000007;
+        // buil graph
+        vector<vector<pair<int, int>>> graph(n + 1);
+        for (const vector<int>& e : edges) {
+            graph[e[0]].push_back({e[1], e[2]});
+            graph[e[1]].push_back({e[0], e[2]});
+        }
+
+        // Dijkstra with calculating number of paths on the go;
+        vector<int> count(n + 1, 0);
+        vector<int> minDist(n + 1, INT_MAX);
+        count[n] = 1;
+        minDist[n] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, n});
+        while (!pq.empty()) {
+            int dist = pq.top().first;
+            int cur = pq.top().second;
+            pq.pop();
+            if (dist != minDist[cur]) continue;
+            for (auto& [nei, cost] : graph[cur]) {
+                if (minDist[nei] > dist + cost) {
+                    minDist[nei] = dist + cost;
+                    pq.push({minDist[nei], nei});
+                    count[nei] = (count[nei] + count[cur]) % MOD;
+                } else if (minDist[nei] > dist) {
+                    count[nei] = (count[nei] + count[cur]) % MOD;
+                }
+            }
+        }  
+        return count[1];
+    }
+};
