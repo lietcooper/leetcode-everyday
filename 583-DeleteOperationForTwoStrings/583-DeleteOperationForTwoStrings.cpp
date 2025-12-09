5730419853f6143385e56333c2115c0138bb18bf
lijunwan <114791855+lietcooper@users.noreply.github.com>
2025-12-09 15:28:16 +0800

    Create 583-DeleteOperationForTwoStrings

---
+++ 583-DeleteOperationForTwoStrings/583-DeleteOperationForTwoStrings.cpp
@@ -0,0 +1,19 @@
+// Last updated: 12/8/2025, 11:28:16 PM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(2, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
+                } else {
+                    dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
+                }
+            }
+        }
+        return m + n - 2 * dp[m % 2][n];
+    }
+};
